09528702eebd3be34e49bd9452c05d2778d09a8b
Daniel Stieber <dstieber@cs.csubak.edu>
2015-04-25 18:07:23 -0700

    background working!

--- parashoot.cpp
+++ parashoot.cpp
@@ -77,7 +77,7 @@ double physicsCountdown=0.0;
 double timeSpan=0.0;
 double timeDiff(struct timespec *start, struct timespec *end) {
     return (double)(end->tv_sec - start->tv_sec ) +
-	(double)(end->tv_nsec - start->tv_nsec) * oobillion;
+        (double)(end->tv_nsec - start->tv_nsec) * oobillion;
 }
 void timeCopy(struct timespec *dest, struct timespec *source) {
     memcpy(dest, source, sizeof(struct timespec));
@@ -98,23 +98,23 @@ int main(void)
 
     //start animation
     while(!done) {
-	while(XPending(dpy)) {
-	    XEvent e;
-	    XNextEvent(dpy, &e);
-	    check_mouse(&e, &game);
-	    check_resize(&game, &e);
-	    done = check_keys(&e, &game);
-	}
-    clock_gettime(CLOCK_REALTIME, &timeCurrent);
-    timeSpan = timeDiff(&timeStart, &timeCurrent);
-    timeCopy(&timeStart, &timeCurrent);
-    physicsCountdown += timeSpan;
-    while(physicsCountdown >= physicsRate) {
-        movement(&game);
-        physicsCountdown -= physicsRate;
-    }
-	render(&game);
-	glXSwapBuffers(dpy, win);
+        while(XPending(dpy)) {
+            XEvent e;
+            XNextEvent(dpy, &e);
+            check_mouse(&e, &game);
+            check_resize(&game, &e);
+            done = check_keys(&e, &game);
+        }
+        clock_gettime(CLOCK_REALTIME, &timeCurrent);
+        timeSpan = timeDiff(&timeStart, &timeCurrent);
+        timeCopy(&timeStart, &timeCurrent);
+        physicsCountdown += timeSpan;
+        while(physicsCountdown >= physicsRate) {
+            movement(&game);
+            physicsCountdown -= physicsRate;
+        }
+        render(&game);
+        glXSwapBuffers(dpy, win);
     }
     cleanupXWindows();
     cleanup_fonts();
@@ -146,23 +146,23 @@ void initXWindows(void) {
     int w=WINDOW_WIDTH, h=WINDOW_HEIGHT;
     dpy = XOpenDisplay(NULL);
     if (dpy == NULL) {  std::cout << "\n\tcannot connect to X server\n" << std::endl;
-	exit(EXIT_FAILURE);
+        exit(EXIT_FAILURE);
     }
     Window root = DefaultRootWindow(dpy);
     XVisualInfo *vi = glXChooseVisual(dpy, 0, att);
     if(vi == NULL) {
-	std::cout << "\n\tno appropriate visual found\n" << std::endl;
-	exit(EXIT_FAILURE);
+        std::cout << "\n\tno appropriate visual found\n" << std::endl;
+        exit(EXIT_FAILURE);
     }
     Colormap cmap = XCreateColormap(dpy, root, vi->visual, AllocNone);
     XSetWindowAttributes swa;
     swa.colormap = cmap;
     swa.event_mask = ExposureMask | KeyPressMask | KeyReleaseMask |
-	ButtonPress | ButtonReleaseMask |
-	PointerMotionMask |
-	StructureNotifyMask | SubstructureNotifyMask;
+        ButtonPress | ButtonReleaseMask |
+        PointerMotionMask |
+        StructureNotifyMask | SubstructureNotifyMask;
     win = XCreateWindow(dpy, root, 0, 0, w, h, 0, vi->depth,
-	    InputOutput, vi->visual, CWColormap | CWEventMask, &swa);
+            InputOutput, vi->visual, CWColormap | CWEventMask, &swa);
     set_title();
     glc = glXCreateContext(dpy, vi, NULL, GL_TRUE);
     glXMakeCurrent(dpy, win, glc);
@@ -197,31 +197,18 @@ unsigned char *buildAlphaData(Ppmimage *img)
     newdata = new unsigned char[img->width * img->height * 4];
     ptr = newdata;
     for (int i=0; i<img->width * img->height * 3; i+=3) {
-	a = *(data+0);
-	b = *(data+1);
-	c = *(data+2);
-	*(ptr+0) = a;
-	*(ptr+1) = b;
-	*(ptr+2) = c;
-	//
-	//get the alpha value
-	//
-	//original code
-	//get largest color component...
-	//*(ptr+3) = (unsigned char)((
-	//		(int)*(ptr+0) +
-	//		(int)*(ptr+1) +
-	//		(int)*(ptr+2)) / 3);
-	//d = a;
-	//if (b >= a && b >= c) d = b;
-	//if (c >= a && c >= b) d = c;
-	//*(ptr+3) = d;
-	//
-	//new code, suggested by Chris Smith, Fall 2013
-	*(ptr+3) = (a|b|c);
-	//
-	ptr += 4;
-	data += 3;
+        a = *(data+0);
+        b = *(data+1);
+        c = *(data+2);
+        *(ptr+0) = a;
+        *(ptr+1) = b;
+        *(ptr+2) = c;
+        //
+        //new code, suggested by Chris Smith, Fall 2013
+        *(ptr+3) = (a|b|c);
+        //
+        ptr += 4;
+        data += 3;
     }
     return newdata;
 }
@@ -235,12 +222,12 @@ void init_opengl(void)
     glMatrixMode(GL_MODELVIEW); glLoadIdentity();
     //Set 2D mode (no perspective)
     glOrtho(0, WINDOW_WIDTH, 0, WINDOW_HEIGHT, -1, 1);
-    
+
     glDisable(GL_LIGHTING);
     glDisable(GL_DEPTH_TEST);
     glDisable(GL_FOG);
     glDisable(GL_CULL_FACE);
-    
+
     //Set the screen background color
     glClearColor(0.1, 0.5, 1.0, 1.0);
     glEnable(GL_TEXTURE_2D);
@@ -257,17 +244,17 @@ void init_opengl(void)
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
     glTexImage2D(GL_TEXTURE_2D, 0, 3, skyImage->width, skyImage->height,
-        0, GL_RGB, GL_UNSIGNED_BYTE, skyImage->data);
+            0, GL_RGB, GL_UNSIGNED_BYTE, skyImage->data);
 }
 
 void check_resize(Game *game, XEvent *e)
 {
     if(e->type != ConfigureNotify)
-	    return;
+        return;
 
     XConfigureEvent xce = e->xconfigure;
     if(xce.width != xres || xce.height != yres) {
-	    reshape_window(game, xce.width, xce.height);
+        reshape_window(game, xce.width, xce.height);
     }
 }
 
@@ -289,26 +276,26 @@ void check_mouse(XEvent *e, Game *game)
     static int n = 0;
 
     if (e->type == ButtonRelease) {
-	return;
+        return;
     }
     if (e->type == ButtonPress) {
-	if (e->xbutton.button==1) {
-	    //Left button was pressed
-	    start_flag = false;
-	    makeParticle(game);
-	    return;
-	}
-	if (e->xbutton.button==3) {
-	    //Right button was pressed
-	    return;
-	}
+        if (e->xbutton.button==1) {
+            //Left button was pressed
+            start_flag = false;
+            makeParticle(game);
+            return;
+        }
+        if (e->xbutton.button==3) {
+            //Right button was pressed
+            return;
+        }
     }
     //Did the mouse move?
     if (savex != e->xbutton.x || savey != e->xbutton.y) {
-	savex = e->xbutton.x;
-	savey = e->xbutton.y;
-	if (++n < 10)
-	    return;
+        savex = e->xbutton.x;
+        savey = e->xbutton.y;
+        if (++n < 10)
+            return;
     }
 }
 
@@ -317,19 +304,19 @@ int check_keys(XEvent *e, Game *game)
     Particle *p;
     //Was there input from the keyboard?
     if (e->type == KeyPress) {
-	int key = XLookupKeysym(&e->xkey, 0);
-	if (key == XK_Escape) {
-	    return 1;
-	}
+        int key = XLookupKeysym(&e->xkey, 0);
+        if (key == XK_Escape) {
+            return 1;
+        }
     }
     p = &game->particle;
     //You may check other keys here.
     if (e->type == KeyPress) {
-	int key = XLookupKeysym(&e->xkey, 0);
-	if (key == XK_Left) {
-	    p->s.center.x -= 16.0; }
-	else if (key == XK_Right) {
-	    p->s.center.x += 16.0; }
+        int key = XLookupKeysym(&e->xkey, 0);
+        if (key == XK_Left) {
+            p->s.center.x -= 16.0; }
+        else if (key == XK_Right) {
+            p->s.center.x += 16.0; }
 
     }
     return 0;
@@ -340,36 +327,36 @@ void movement(Game *game)
     Particle *p;
 
     if (game->n <= 0)
-	return;
+        return;
 
     p = &game->particle;
     /*p->s.center.x += p->velocity.x;
       p->s.center.y += p->velocity.y;
       p->s.center.y -= GRAVITY;
-      */
+     */
     //check for collision with shapes...
     //Shape *s;
 
 
     //check for off-screen
     if (p->s.center.y < 0.0) {
-	game->n = 0;
+        game->n = 0;
     }
     if(size_flag == true){
-	if(p->s.center.x < 0.0){
-	    p->s.center.x += xres;
-	}
-	else if(p->s.center.x > xres) {
-	    p->s.center.x -= xres;
-	}
+        if(p->s.center.x < 0.0){
+            p->s.center.x += xres;
+        }
+        else if(p->s.center.x > xres) {
+            p->s.center.x -= xres;
+        }
     }
     else if(size_flag == false){
-	if(p->s.center.x < 0.0) {
-	    p->s.center.x += WINDOW_WIDTH;
-	}
-	else if(p->s.center.x > WINDOW_WIDTH) {
-	    p->s.center.x -= WINDOW_WIDTH;
-	}
+        if(p->s.center.x < 0.0) {
+            p->s.center.x += WINDOW_WIDTH;
+        }
+        else if(p->s.center.x > WINDOW_WIDTH) {
+            p->s.center.x -= WINDOW_WIDTH;
+        }
     }
 }
 
@@ -387,48 +374,49 @@ void render(Game *game)
         glTexCoord2f(1.0f, 1.0f); glVertex2i(xres,0);
         glEnd();
     }
-    float w, h;
-    glClear(GL_COLOR_BUFFER_BIT);
-    //draw character here
-    glPushMatrix();
-    glColor3ub(125,0,220);
-    Vec *c = &game->particle.s.center;
-    w = 12;
-    h = 12;
-    glBegin(GL_QUADS);
-    glVertex2i(c->x-w, c->y-h);
-    glVertex2i(c->x-w, c->y+h);
-    glVertex2i(c->x+w, c->y+h);
-    glVertex2i(c->x+w, c->y-h);
-    glEnd();
-    glPopMatrix();
-    
-    if(start_flag)
-    {
-    Rect start;
-    Rect click;
 
-    start.centerx = xres/2;
-    start.centery = yres/2 + 400;
-    start.bot = yres/2 + 400;
-    start.width = 500;
-    start.height = 100;
-    start.center = xres/2 + 400;
-    start.left = start.centerx;
-    start.right = start.centerx;
-    start.top = yres/2 + 400;
+    if(!start_flag) {
+        float w, h;
+        //draw character here
+        glPushMatrix();
+        glColor3ub(125,0,220);
+        Vec *c = &game->particle.s.center;
+        w = 12;
+        h = 12;
+        glBegin(GL_QUADS);
+        glVertex2i(c->x-w, c->y-h);
+        glVertex2i(c->x-w, c->y+h);
+        glVertex2i(c->x+w, c->y+h);
+        glVertex2i(c->x+w, c->y-h);
+        glEnd();
+        glPopMatrix();
+    }
 
-    click.centerx = xres/2;
-    click.centery = yres/2 + 375;
-    click.bot = yres/2 + 375;
-    click.width = 500;
-    click.height = 100;
-    click.center = xres/2 + 375;
-    click.left = click.centerx;
-    click.right = click.centerx;
-    click.top = yres/2 + 375;
-    
-    ggprint16(&start, 1000, 0x00fff000, "PARASHOOT!");
-    ggprint16(&click, 1000, 0x00fff000, "Click to start");
+    if(start_flag) {
+        Rect start;
+        Rect click;
+
+        start.centerx = xres/2;
+        start.centery = yres/2 + 200;
+        start.bot = yres/2 + 200;
+        start.width = 500;
+        start.height = 100;
+        start.center = xres/2 + 200;
+        start.left = start.centerx;
+        start.right = start.centerx;
+        start.top = yres/2 + 200;
+
+        click.centerx = xres/2;
+        click.centery = yres/2;
+        click.bot = yres/2;
+        click.width = 500;
+        click.height = 100;
+        click.center = xres/2;
+        click.left = click.centerx;
+        click.right = click.centerx;
+        click.top = yres/2;
+
+        ggprint16(&start, 1000, 0x00fff000, "PARASHOOT!");
+        ggprint16(&click, 1000, 0x00fff000, "Click to start");
     }
 }
